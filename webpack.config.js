63b13ed5f684ec3eed86a6ef1ca5e70b0b32f40a
korbinm <81394169+korbinm@users.noreply.github.com>
2022-01-25 22:16:13 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules\/(?!()\/).*/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
+            {
+                test:/\.(gif|png|jpe?g|svg)$/i,
+                use: {
+                    loader: 'file-loader'
+                }
+
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html",
+        }),
+    ],
+    devServer: {
+        historyApiFallback: true,
+    },
+    resolve: {
+        alias: {
+            "react-native": "react-native-web"
+        },
+        extensions: [".web.js",".js",".jsx",".ts",".tsx"]
+    },
+};
+// module.exports = {
+//     entry: path.resolve(__dirname, 'src') + '/index.js',
+//     output: {
+//         path: path.resolve(__dirname, 'dist') + '/src',
+//         filename: 'bundle.js',
+//         publicPath: '/src/'
+//     },
+//     resolve: {
+//         extensions: [".mjs", ".js"],
+//         fallback: {
+//             "path": require.resolve("path-browserify")
+//         }
+//     },
+//     plugins: [
+//         new Dotenv()
+//     ]
+// }
