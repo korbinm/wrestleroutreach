ad06690a0700758e0f896004d1a0d84f0194bc97
Hoppykid1 <akcalebh@gmail.com>
2022-04-06 11:07:31 -0400

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react/cjs/react.production.min";
+import '../App.css';
+
+function Dashboard() {
+
+
+  return(
+        <div id="services">
+            <div id="info">
+                <h3>As a Member</h3>
+                <p>You are able to send a video within a minute time frame. From it being filmed on your basement mats
+                    to
+                    a competition review, anything is accepted. Our guys will review, listen to your questions you have
+                    attached
+                    with the video and create a video response back showing technique for your question. Each submitted
+                    video
+                    will
+                    be $20, charged once a video reply is sent.</p>
+            </div>
+        </div>
+    )
+};
+export default Dashboard;
