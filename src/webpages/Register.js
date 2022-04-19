5bf9026d77ac774f054f6a38acd3185a3e4b49fb
korbinm <81394169+korbinm@users.noreply.github.com>
2022-04-19 13:22:59 -0400

    Restyled

--- src/webpages/Register.js
+++ src/webpages/Register.js
@@ -1,125 +1,121 @@
 import React from 'react';
-import {useState } from 'react';
+import {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {createCustomer} from "../api";
-import App from "../index.js";
-import {Route, Redirect} from "react-router-dom"
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import App from "../index";
+import Layout from "../webpages/Layout.js";
+import Home from "./home";
 
 
 //Form for registering
 function RegisterForm() {
-    let isValidated = false;
     const [inputs, setInputs] = useState({
         name: "",
-        email:"",
-        password:"",
+        email: "",
+        password: "",
         confirm_pass: "",
-        errors:""
+        errors: ""
     });
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({...values,[name]: value}))
+        setInputs(values => ({...values, [name]: value}))
     }
     //validate user registration information
-    const validate = () =>{
-        if(!inputs.name){
+    const validate = () => {
+        if (!inputs.name) {
             inputs.errors = "Please enter a name.";
             return false;
         }
-        if(!inputs.email){
+        if (!inputs.email) {
             inputs.errors = "Please enter an email.";
             return false;
         }
-        if(!inputs.password){
+        if (!inputs.password) {
             inputs.errors = "Please enter a password.";
             return false;
         }
         //insert valid password requirement check
-        if(!inputs.confirm_pass){
+        if (!inputs.confirm_pass) {
             inputs.errors = "Please enter a confirmation password";
             return false;
         }
-        if(inputs.password !== inputs.confirm_pass){
+        if (inputs.password !== inputs.confirm_pass) {
             inputs.errors = "Passwords do not match!"
             return false;
         }
         return true;
     }
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         event.preventDefault();
-        if(!validate()){
+        if (!validate()) {
             alert(inputs.errors);
-        }
-        else{
-            createCustomer(inputs).then(res =>{
-                isValidated = true;
-                console.log('user added');
-
+        } else {
+            createCustomer(inputs).then(res => {
+                console.log("User Created");
             })
         }
-
-
     }
 
-    return(
-<form id="register" onSubmit={handleSubmit} >
-	<div>
-		<label>Full Name</label>
-    </div>
-	<div>
-				<input
-                    type = "text"
-                    name = "name"
+    return (
+        <form id="register" onSubmit={handleSubmit}>
+            <div>
+                <label>Full Name</label>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    name="name"
                     value={inputs.name || ""}
                     onChange={handleChange}
                 />
-	</div>
-	<div>
-            <label>Email</label>
-	</div>
-	<div>
+            </div>
+            <div>
+                <label>Email</label>
+            </div>
+            <div>
                 <input
-                    type = "email"
-                    name = "email"
-                    value = {inputs.email || ""}
+                    type="email"
+                    name="email"
+                    value={inputs.email || ""}
                     onChange={handleChange}
                 />
-	</div>
-	<div>
-            <label>Password</label>
-	</div>
-	<div>
-                <input
-                    type = "password"
-                    name = "password"
-                    value = {inputs.password || ""}
-                    onChange={handleChange}
-                />
-
-	</div>
-	<div>
-            <label>Confirm Password</label>
-	</div>
-	<div>
+            </div>
+            <div>
+                <label>Password</label>
+            </div>
+            <div>
                 <input
                     type="password"
-                    name = "confirm_pass"
-                    value = {inputs.confirm_pass || ""}
+                    name="password"
+                    value={inputs.password || ""}
                     onChange={handleChange}
                 />
 
-        <div>
-			<input
-                type = "submit"
-                value="Submit"
-            />
+            </div>
+            <div>
+                <label>Confirm Password</label>
+            </div>
+            <div>
+                <input
+                    type="password"
+                    name="confirm_pass"
+                    value={inputs.confirm_pass || ""}
+                    onChange={handleChange}
+                />
 
-        </div>
-	</div>
-</form>
+                <div>
+                    <input
+                        type="submit"
+                        value="Submit"
+                    />
+                </div>
+            </div>
+        </form>
     )
 }
+
 const Register = () => {
     return RegisterForm();
 }
