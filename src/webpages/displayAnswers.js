791d1bd9d0b06c884891bdead155ee29dd76ef06
korbinm <81394169+korbinm@users.noreply.github.com>
2022-04-26 13:06:15 -0400

    Dashboard

--- src/webpages/displayAnswers.js
+++ src/webpages/displayAnswers.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../App.css";
 import {useAuth0} from "@auth0/auth0-react"
+import {useNavigate} from "react-router-dom"
 
 function Display({customerVideo, responseVideo, notes,answered}) {
     const {user} = useAuth0();
     let manager = false
+    const navigate = useNavigate();
 
     let cNotes =notes.substring(
 
@@ -19,8 +21,10 @@ function Display({customerVideo, responseVideo, notes,answered}) {
                     </td>
                     <td>
                         {answered ?
-                        <iframe width="420" height ="315" src={responseVideo}>Customer's Question</iframe> :
+                            <iframe width="420" height ="315" src={responseVideo}>Customer's Question</iframe>
+                        :
                             <p>There has not been a response yet!</p>}
+                        <button onClick={()=>{navigate("/Upload")}}>Test </button>
                     </td>
 </div>
     );
